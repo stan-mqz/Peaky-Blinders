cd80cd5da1f72c7270a4647dd9de043b88a70968
stan-mqz <moscarstanley37@gmail.com>
2024-09-19 11:47:05 -0600

    js added

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('.formulario')
+const input = document.querySelector('.formulario__campo')
+const icon = document.querySelector('.formulario__icono')
+const clean = document.querySelector('.formulario__limpiar')
+
+input.addEventListener('input', () => {
+    
+    if (input.value !== '') {
+        
+        icon.classList.add('hidden')
+        clean.classList.remove('hidden')
+
+    }  else {
+        
+         icon.classList.remove('hidden')
+         clean.classList.add('hidden')
+
+    }
+
+})
+
+clean.addEventListener('click', () => {
+    input.value = ''
+})
