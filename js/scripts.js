2b7f35fb7a2499069522c458068925412ebba22b
stan-mqz <moscarstanley37@gmail.com>
2024-10-09 07:38:47 -0600

    Created 'js' folder for JavaScript files

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,23 @@ const icon = document.querySelector('.formulario__icono')
 const clean = document.querySelector('.formulario__limpiar')
 
 input.addEventListener('input', () => {
-    
+
     if (input.value !== '') {
-        
+
         icon.classList.add('hidden')
         clean.classList.remove('hidden')
 
-    }  else {
-        
-         icon.classList.remove('hidden')
-         clean.classList.add('hidden')
+    } else {
+
+        icon.classList.remove('hidden')
+        clean.classList.add('hidden')
 
     }
 
 })
 
 clean.addEventListener('click', () => {
-    input.value = ''
+        input.value = ''
+        icon.classList.remove('hidden')
+        clean.classList.add('hidden')
 })
